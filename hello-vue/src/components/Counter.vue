5e3b168bcdd469bb833bfb5a1e7bc47a903bb837
sheylax <blizzard00666@gmail.com>
2019-04-03 16:37:49 +0200

    creat a click counter in cli

---
+++ hello-vue/src/components/Counter.vue
@@ -0,0 +1,16 @@
+<template lang="html">
+  <button type="button" @click="counter++">{{ counter }}</button>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      counter : 0
+    }
+  }
+}
+</script>
+
+<style lang="css" scoped>
+</style>
